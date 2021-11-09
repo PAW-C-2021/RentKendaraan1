2df076c859c466e9192d11fe269e8172f1bb9c70
DamasHimawan <damas261100@gmail.com>
2021-11-09 21:13:08 +0700

    Add project files.

---
+++ RentKendaraan1/Models/JenisKendaraan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentKendaraan1.Models
+{
+    public partial class JenisKendaraan
+    {
+        public JenisKendaraan()
+        {
+            Kendaraan = new HashSet<Kendaraan>();
+        }
+
+        public int IdJenisKendaraan { get; set; }
+        public string NamaJenisKendaraan { get; set; }
+
+        public virtual ICollection<Kendaraan> Kendaraan { get; set; }
+    }
+}
